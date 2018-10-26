e58c20ca1eaf709f0d4ddd963a8b1bb2ff90dc77
Artur <artik1999@wp.pl>
2018-10-26 19:18:45 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+
+:root {
+  --primary: #eb3f57;
+  --secondary: #515067
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  min-height: 100vh;
+  color: #2c3e50;
+  background-color: var(--secondary);
+}
+</style>
